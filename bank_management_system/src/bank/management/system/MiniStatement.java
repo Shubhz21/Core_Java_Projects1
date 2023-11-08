af390d0292aa6e12e429b92e637ba0d67488979e
shubham funde <shubhamfunde2222@gmail.com>
2023-11-08 16:09:05 +0530

    8nov23(16:09)

--- bank_management_system/src/bank/management/system/MiniStatement.java
+++ bank_management_system/src/bank/management/system/MiniStatement.java
@@ -3,78 +3,96 @@ package bank.management.system;
 import javax.swing.*;
 import java.awt.*;
 import java.sql.*;
+import java.awt.event.*;
 /**
  *
  * @author Shubham Funde
  */
-public class MiniStatement extends JFrame{
+public class MiniStatement extends JFrame implements ActionListener{
     
-    //String pinnumber;
-    JLabel bank,mini,card, balance;
     
+
+String pinnumber;
+JButton b1, b2;
+    JLabel l1,l4;
     MiniStatement(String pinnumber){
-//        this.pinnumber=pinnumber;
+        super("Mini Statement");
+        getContentPane().setBackground(Color.WHITE);
+        setSize(400,600);
+        setLocation(20,20);
+        this.pinnumber=pinnumber;
         
-        setTitle("Mini Statement");
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/klogo.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(150,50,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel label = new JLabel(i3);
+        label.setBounds(70,10,150,50);
+        add(label);
+        
+        l1 = new JLabel();
+        l1.setBounds(20, 120, 350, 280);
+        add(l1);
+        
+        JLabel l2 = new JLabel("Kotak Mahindra Bank");
+        l2.setBounds(140, 70, 200, 20);
+        add(l2);
+        
+        JLabel l3 = new JLabel();
+        l3.setBounds(20, 100, 300, 20);
+        add(l3);
+        
+        JLabel l4 = new JLabel();
+        l4.setBounds(20, 420, 300, 20);
+        add(l4);
+        
+        try{
+            Conn c = new Conn();
+            ResultSet rs = c.s.executeQuery("select * from login where pin = '"+pinnumber+"'");
+            while(rs.next()){
+                l3.setText("Card Number:    " + rs.getString("cardnumber").substring(0, 4) + "XXXXXXXX" + rs.getString("cardnumber").substring(12));
+            }
+        }catch(Exception e){}
+        	 
+        try{
+            int balance = 0;
+            
+            Conn c1  = new Conn();
+            ResultSet rs = c1.s.executeQuery("SELECT * FROM bank where pin = '"+pinnumber+"'");
+            while(rs.next()){
+                l1.setText(l1.getText() + "<html>"+rs.getString("date")+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + rs.getString("type") + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + rs.getString("money") + "<br><html>");
+                if(rs.getString("type").equals("Deposit")){
+                    balance += Integer.parseInt(rs.getString("money"));
+                }else{
+                    balance -= Integer.parseInt(rs.getString("money"));
+                }
+            }
+            l4.setText("Your total Balance is Rs "+balance);
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         
         setLayout(null);
         
+        b1 = new JButton("Exit");
+        b1.addActionListener(this);
+        b1.setBounds(20, 500, 100, 25);
+        add(b1);
         
         
-         bank = new JLabel("Kotak Mahindra Bank");
-        bank.setBounds(148,20,200,20);
-        add(bank);
+    }
+    public void actionPerformed(ActionEvent ae){
         
-         mini = new JLabel();
-        mini.setBounds(20,140,300,40);
-        add(mini);
+        this.setVisible(false);
         
-         card = new JLabel();
-        card.setBounds(20,100,200,20);
-        add(card);
+            
         
-        balance= new JLabel();
-        balance.setBounds(20,400,300,20);
-        add(balance);
-        
-        try{
-            Conn conn = new Conn();
-            ResultSet rs = conn.s.executeQuery("select * from login where pin ='"+pinnumber+"'");
-            while(rs.next()){
-                card.setText("Card Number: "+ rs.getString("cardnumber").substring(0,4)+"XXXXXXXX" + rs.getString("cardnumber").substring(12));
-            }
-        }catch(Exception e){
-            System.out.println(e);
-        }
-        
-        try{
-            Conn conn = new Conn();
-            int bal = 0;
-            ResultSet rs= conn.s.executeQuery("select * from bank where pin ='"+pinnumber+"'");
-            while(rs.next()){
-                mini.setText(mini.getText()+ "<html>"+rs.getString("date")+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+ rs.getString("type") + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+ rs.getString("money")+ "<br><br></html>");
-                
-                if(rs.getString("type").equals("Deposit")){
-                        bal += Integer.parseInt(rs.getString("money"));
-                    }else{
-                        bal -= Integer.parseInt(rs.getString("money"));
-                    }
-            }
-            balance.setText("Your Current Account Balance is Rs. "+bal);
-        }catch(Exception e){
-            System.out.println(e);
-        }
-        
-        
-        
-        
-        setSize(400,700);
-        setLocation(20,20);
-        getContentPane().setBackground(Color.white);
-        setVisible(true);
+                   
+    }
+
+    
+    public static void main(String[] args){
+        new MiniStatement("").setVisible(true);
     }
     
-    public static void main(String []args){
-        new MiniStatement("");
-    }
 }
